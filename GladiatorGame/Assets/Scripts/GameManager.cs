bfdc8aa4ee1c02b56f4b6fc2702c6feab4372d10
TakayoshiOgawa <ogawa4283gamecreator@gmail.com>
2017-09-06 14:27:19 +0900

    Maked GameManager(include GamTimeText and KillSilder)

---
+++ GladiatorGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : SingletonMonoBehaviour<GameManager> {
+
+    private float timer;
+
+    [SerializeField]
+    private Text time;
+
+    protected override void Awake() {
+        base.Awake();
+    }
+
+    // Use this for initialization
+    void Start () {
+        timer = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time.text = "Time:" + (Time.time - timer).ToString("f1");
+	}
+}
