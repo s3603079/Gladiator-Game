d5fa11d6fc51a11b54c1a76bf43b08c35600c7fd
TakayoshiOgawa <ogawa4283gamecreator@gmail.com>
2017-09-07 11:55:22 +0900

    GameManager have Gladiator(Charactor) and Controll's

--- GladiatorGame/Assets/Scripts/GameManager.cs
+++ GladiatorGame/Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using GamepadInput;
 
 public class GameManager : SingletonMonoBehaviour<GameManager> {
 
@@ -9,6 +10,11 @@ public class GameManager : SingletonMonoBehaviour<GameManager> {
 
     [SerializeField]
     private Text time;
+
+    [SerializeField]
+    private Gladiator player1;
+    [SerializeField]
+    private Gladiator player2;
 
     protected override void Awake() {
         base.Awake();
@@ -22,5 +28,19 @@ public class GameManager : SingletonMonoBehaviour<GameManager> {
 	// Update is called once per frame
 	void Update () {
         time.text = "Time:" + (Time.time - timer).ToString("f1");
-	}
+
+        if (player1)
+        {
+            var pad1 = GamePad.GetState(GamePad.Index.One);
+            player1.Walk(pad1.LeftStickAxis.x);
+            player1.Jump(pad1.A);
+        }
+
+        if (player2)
+        {
+            var pad2 = GamePad.GetState(GamePad.Index.Two);
+            player2.Walk(pad2.LeftStickAxis.x);
+            player2.Jump(pad2.A);
+        }
+    }
 }
---
+++ GladiatorGame/Assets/Scripts/Gladiator.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class Gladiator : MonoBehaviour {
+
+    [SerializeField]
+    private float walkSpeed = 1F;
+    [SerializeField]
+    private float jumpPower = 1F;
+
+    private Rigidbody2D rb2d;
+    private Vector3 velocity;
+    private bool isGrounded;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.constraints = RigidbodyConstraints2D.FreezeRotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var mPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //if (transform.position.x <= mPos.x)
+        //{
+        //    transform.localScale = new Vector2(+1F, transform.localScale.y);
+        //}
+        //else if (transform.position.x >= mPos.x)
+        //{
+        //    transform.localScale = new Vector2(-1F, transform.localScale.y);
+        //}
+    }
+
+    public void Walk(float inputValue) {
+        rb2d.velocity = new Vector2(inputValue * walkSpeed, rb2d.velocity.y);
+    }
+
+    public void Jump(bool InputTrigger) {
+        if(InputTrigger && isGrounded)
+        {
+            isGrounded = false;
+            rb2d.AddForce(transform.up * jumpPower * 200F);
+        }
+    }
+
+    public void Attack() {
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.transform.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision) {
+        if (collision.transform.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
+}
