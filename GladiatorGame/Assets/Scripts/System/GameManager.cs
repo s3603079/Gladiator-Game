690d2162499070b167bd4ec8bf1c3edef74a9e33
TakayoshiOgawa <ogawa4283gamecreator@gmail.com>
2017-09-08 12:13:48 +0900

    Fixed MergeProject XD

--- GladiatorGame/Assets/Scripts/System/GameManager.cs
+++ GladiatorGame/Assets/Scripts/System/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using GamepadInput;
 
 public class GameManager : SingletonMonoBehaviour<GameManager> {
 
@@ -9,6 +10,10 @@ public class GameManager : SingletonMonoBehaviour<GameManager> {
 
     [SerializeField]
     private Text time;
+    [SerializeField]
+    private Gladiator player1;
+    [SerializeField]
+    private Gladiator player2;
 
     protected override void Awake() {
         base.Awake();
@@ -22,21 +27,39 @@ public class GameManager : SingletonMonoBehaviour<GameManager> {
 	// Update is called once per frame
 	void Update () {
         time.text = "Time:" + (Time.time - timer).ToString("f1");
-	}
 
-<<<<<<< HEAD:GladiatorGame/Assets/Scripts/System/GameManager.cs
+        SettingGladiator(player1, GamePad.Index.One);
+        SettingGladiator(player2, GamePad.Index.Two);
+
+        //if (player1 != null)
+        //{
+        //    var pad = GamePad.Index.One;
+        //    player1.Walk(GamePad.GetAxis(GamePad.Axis.LeftStick, pad).x);
+        //    player1.Jump(GamePad.GetButtonDown(GamePad.Button.A, pad));
+        //    player1.RotaShoulder(GamePad.GetAxis(GamePad.Axis.RightStick, pad));
+        //    player1.Attack(GamePad.GetTrigger(GamePad.Trigger.RightTrigger, pad));
+        //}
+
+        //if (player2 != null)
+        //{
+        //    var pad = GamePad.Index.Two;
+        //    player2.Walk(GamePad.GetAxis(GamePad.Axis.LeftStick, pad).x);
+        //    player2.Jump(GamePad.GetButtonDown(GamePad.Button.A, pad));
+        //    player2.RotaShoulder(GamePad.GetAxis(GamePad.Axis.RightStick, pad));
+        //    player2.Attack(GamePad.GetTrigger(GamePad.Trigger.RightTrigger, pad));
+        //}
+    }
+
+    void SettingGladiator(Gladiator player, GamePad.Index index) {
+        if (player == null) return;
+        player.Walk(GamePad.GetAxis(GamePad.Axis.LeftStick, index).x);
+        player.Jump(GamePad.GetButtonDown(GamePad.Button.A, index));
+        player.RotaShoulder(GamePad.GetAxis(GamePad.Axis.RightStick, index));
+        player.Attack(GamePad.GetTrigger(GamePad.Trigger.RightTrigger, index));
+    }
+
     public float GetTime()
     {
         return Time.time - timer;
-=======
-        if (player1)
-        {
-            var pad = GamePad.Index.One;
-            player1.Walk(GamePad.GetAxis(GamePad.Axis.LeftStick, pad).x);
-            player1.Jump(GamePad.GetButtonDown(GamePad.Button.A, pad));
-            player1.RotaShoulder(GamePad.GetAxis(GamePad.Axis.RightStick, pad));
-            player1.Attack(GamePad.GetTrigger(GamePad.Trigger.RightTrigger, pad));
-        }
->>>>>>> feature/Player:GladiatorGame/Assets/Scripts/GameManager.cs
     }
 }
---
+++ GladiatorGame/Assets/Scripts/sceneManger.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneManger : MonoBehaviour {
+
+    Scene currentScene;    
+    string previousScene;
+    public Object thisObject;
+    public string titleSceneName;
+    public string singlePlayerSceneName;
+    public string multiPlayerSceneName;
+    public string gameOverSceneName;
+
+    // Use this for initialization
+    void Start () {
+        Object.DontDestroyOnLoad(thisObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        currentScene = SceneManager.GetActiveScene();
+
+        //quits
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            Application.Quit(); 
+        }
+
+        if (string.Equals(currentScene.name, "title"))
+        {
+            titleScene();
+        }
+
+        else if (string.Equals(currentScene.name, "arenaSingle"))
+        {
+            singleScene();
+        }
+
+        else if (string.Equals(currentScene.name, "arenaMulti"))
+        {
+            mulltiScene();
+        }
+
+        else if (string.Equals(currentScene.name, "gameOver"))
+        {
+            gameOverScene();
+        }
+    }
+
+    void titleScene() 
+    {
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SceneManager.LoadScene(singlePlayerSceneName);
+        }
+        else if (Input.GetKeyDown(KeyCode.M))
+        {
+            SceneManager.LoadScene(multiPlayerSceneName);
+        }
+    }
+
+    void singleScene()
+    {
+        previousScene = singlePlayerSceneName;
+
+        if (Input.GetKeyDown(KeyCode.M))//player is dead
+        {
+            SceneManager.LoadScene(gameOverSceneName);
+        }
+    }
+
+    void mulltiScene()
+    {
+        previousScene = multiPlayerSceneName;
+
+        if (Input.GetKeyDown(KeyCode.M))//player wins
+        {
+            SceneManager.LoadScene(gameOverSceneName);
+        }
+    }
+
+    void gameOverScene() {
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(previousScene);
+        }
+        else if (Input.GetKeyDown(KeyCode.T))
+        {
+            SceneManager.LoadScene(titleSceneName);
+        }
+    }
+}
